ae42f209a264ad38a45fe81c70c652e44cf3c5b8
Edmund Prager <eprager@Edmunds-MacBook-Pro.local>
2022-05-16 12:43:04 +0100

    fixing merge issues

--- ramp_filter.py
+++ ramp_filter.py
@@ -13,8 +13,7 @@ def ramp_filter(sinogram, scale, alpha=0.001):
     cosine raised to the power given by alpha."""
 
     # get input dimensions
-    angles = sinogram.shape[0]
-    n = sinogram.shape[1]
+    (angles, n) = sinogram.shape
 
     # Set up filter to be at least twice as long as input
     m = np.ceil(np.log(2 * n - 1) / np.log(2))
@@ -22,8 +21,9 @@ def ramp_filter(sinogram, scale, alpha=0.001):
 
     # apply filter to all angles
     print("Ramp filtering")
-    #define filter
+    # define filter
     omega_max = 2*np.pi/scale
+
     freq = np.linspace(-omega_max,omega_max,m)
     filter = np.abs(freq)/(2*np.pi)*np.cos(freq/omega_max*np.pi/2)**alpha
     for i in range(angles):
@@ -32,6 +32,7 @@ def ramp_filter(sinogram, scale, alpha=0.001):
         filt_ft = filter*sin_line_ft
         filt_ft = np.fft.ifftshift(filt_ft)
         sinogram[i] = np.real(np.fft.ifft(filt_ft)[0:n])
+
 
 
     return sinogram
