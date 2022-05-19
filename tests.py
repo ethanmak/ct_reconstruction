2a3d3044086235840deffb783a4519208b9c9562
Gleb Vinnychenko <hv275@cam.ac.uk>
2022-05-19 09:47:19 +0100

    Visual reconstruction tests added

--- tests.py
+++ tests.py
@@ -6,6 +6,7 @@ from ct_phantom import *
 from ct_lib import *
 from scan_and_reconstruct import *
 from create_dicom import *
+import matplotlib.pyplot as plt
 
 def test_ct_calibrate(material, source):
 	# tests 'ct_calibrate'
@@ -36,4 +37,27 @@ def run_tests(material, source):
 	test_ct_calibrate(material, source)
 	print("Done with 'ct_calibrate'.")
 	
-	
+
+
+def test_scan_and_reconstruct(material,source):
+	"""
+	Function to create a reconstruction based on a phantom and display them side by side for checking.
+	The plots are drawn and saved side by side to ensure easy comparison
+	"""
+	fig, ax = plt.subplots(1,2)
+
+	ax[0].axis("off")
+	ax[1].axis("off")
+
+	photons = source.photon('80kVp, 2mm Al')
+	scale = 0.1
+	phantom = ct_phantom(material.name,256,3,'Titanium')
+	scan = scan_and_reconstruct(photons,material,phantom,scale,angles = 256)
+	ax[0].imshow(phantom, cmap = "Greys_r")
+	ax[0].set_aspect("equal","box")
+	ax[0].set_title("Original Phantom")
+	ax[1].imshow(scan, cmap = "Greys_r")
+	ax[1].set_aspect("equal","box")
+	ax[1].set_title("Reconstructed Image")
+	plt.savefig("saved_results/test_scan_and_reconstruct.png")
+	plt.show()
