1b217d5a32f2c7c18e87aed9cc43a525a61aa78f
Edmund Prager <eprager@Edmunds-MacBook-Pro.local>
2022-05-19 13:58:55 +0100

    Made a few corrections in 'ramp_filter.py' and better commented 'back_project.py'

--- back_project.py
+++ back_project.py
@@ -37,12 +37,15 @@ def back_project(sinogram, skip=1):
         # interpolate and add this data to output
         # remembering to multiply by dtheta as well as sum
         # Either of the following options will work
-        x2 = scipy.interpolate.interp1d(np.arange(0, ns, 1), sinogram[angle], kind='linear', copy=False, assume_sorted=True, bounds_error=False, fill_value=0, axis=0)
+        # -----
+		# x2 = scipy.interpolate.interp1d(np.arange(0, ns, 1), sinogram[angle], kind='linear', copy=False, assume_sorted=True, bounds_error=False, fill_value=0, axis=0)
 		# reconstruction = reconstruction + x2(x0) * (math.pi / angles)
+        # -----
         x2 = scipy.ndimage.map_coordinates(
             sinogram[angle], [x0], order=1, mode="constant", cval=0, prefilter=False
         )
         reconstruction = reconstruction + x2 * (math.pi / angles)
+        # -----
 
     # ensure any data outside the reconstructed circle is set to invalid
     reconstruction[np.where((xi**2 + yi**2) > (ns / 2) ** 2)] = -1
--- ramp_filter.py
+++ ramp_filter.py
@@ -2,37 +2,36 @@ import math
 import numpy as np
 import numpy.matlib
 
-
 def ramp_filter(sinogram, scale, alpha=0.001):
-    """Ram-Lak filter with raised-cosine for CT reconstruction
+	"""Ram-Lak filter with raised-cosine for CT reconstruction
 
-    fs = ramp_filter(sinogram, scale) filters the input in sinogram (angles x samples)
-    using a Ram-Lak filter.
+	fs = ramp_filter(sinogram, scale) filters the input in sinogram (angles x samples)
+	using a Ram-Lak filter.
 
-    fs = ramp_filter(sinogram, scale, alpha) can be used to modify the Ram-Lak filter by a
-    cosine raised to the power given by alpha."""
+	fs = ramp_filter(sinogram, scale, alpha) can be used to modify the Ram-Lak filter by a
+	cosine raised to the power given by alpha."""
 
-    # get input dimensions
-    (angles, n) = sinogram.shape
+	# get input dimensions
+	(angles, n) = sinogram.shape
 
-    # Set up filter to be at least twice as long as input
-    m = np.ceil(np.log(2 * n - 1) / np.log(2))
-    m = int(2**m)
+	# Set up filter to be at least twice as long as input
+	#m = np.ceil(np.log(2 * n - 1) / np.log(2))
+	#m = int(2**m)
 
-    # apply filter to all angles
-    print("Ramp filtering")
-    # define filter
-    omega_max = 2*np.pi/scale
+	# apply filter to all angles
+	print("Ramp filtering")
+	# define filter
+	omega_max = np.pi/scale # factor of 2 taken out according to 3G4 notes: omega_max of sinogram will be half that of omega_max of sinogram padded to length m with zeros
+	
+	freq = np.linspace(-omega_max, omega_max, 2*n, endpoint=False)
+	filt = np.abs(freq)/(2*np.pi)*np.cos(freq/omega_max*np.pi*0.5)**alpha
+	filt[n] = filt[n + 1] / 6.0 # value at k=0 changed to one sixth that at k=1 according to handout
+	
+	for i in range(angles):
+		#take fft for value
+		sin_line_ft = np.fft.fftshift(np.fft.fft(sinogram[i], 2*n, norm="backward"))
+		filt_ft = filt*sin_line_ft
+		filt_ft = np.fft.ifftshift(filt_ft)
+		sinogram[i] = np.real(np.fft.ifft(filt_ft, norm="backward")[0:n])
 
-    freq = np.linspace(-omega_max,omega_max,m)
-    filter = np.abs(freq)/(2*np.pi)*np.cos(freq/omega_max*np.pi/2)**alpha
-    for i in range(angles):
-        #take fft for value
-        sin_line_ft = np.fft.fftshift(np.fft.fft(sinogram[i],m,norm="backward"))
-        filt_ft = filter*sin_line_ft
-        filt_ft = np.fft.ifftshift(filt_ft)
-        sinogram[i] = np.real(np.fft.ifft(filt_ft,norm = "backward")[0:n])
-
-
-
-    return sinogram
+	return sinogram
