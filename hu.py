f4a04ab2446893117e455f508e978201fc4f6e2b
Edmund Prager <pragere12@gmail.com>
2022-05-23 12:14:39 +0100

    Added Houndsfield Unit normalisation to hu.py.

--- hu.py
+++ hu.py
@@ -2,29 +2,22 @@ import numpy as np
 from attenuate import *
 from ct_calibrate import *
 
-
-def hu(p, material, reconstruction, scale):
-    """convert CT reconstruction output to Hounsfield Units
-    calibrated = hu(p, material, reconstruction, scale) converts the reconstruction into Hounsfield
-    Units, using the material coefficients, photon energy p and scale given."""
-
-    # use water to calibrate
-
-    # put this through the same calibration process as the normal CT data
-
-    # use result to convert to hounsfield units
-    # limit minimum to -1024, which is normal for CT data.
-    
-	# get average photon energy of source:
-	energy_sum = 0.0
-	for (i, v) in enumerate(photons):
-		energy_sum += (i + 1.0)*v
-	energy_index = int(np.round(energy_sum/sum(photons))) - 1
-	# get corresponding attenuation coefficients of water and air:
-	# the coeffs array gives attenuation coefficients in intervals of 0.001 MeV or 1.0 keV, starting from 0.001 [MeV]. So, the value at index 'keVp - 1' is the the attenuation coefficient of the material for photons of energy $keVp [keVp].
-	mu_water = material.coeff("Water")[energy_index]
-	mu_air = material.coeff("Air")[energy_index]
-	# apply normalisation:
-	back_proj = 1000.0*(back_proj - mu_water)/(mu_water - mu_air)
-
-    return reconstruction
+def hu(photons, material, reconstruction, n_detectors, scale):
+	"""convert CT reconstruction output to Hounsfield Units
+	calibrated = hu(photons, material, reconstruction, n_detectors, scale) converts the reconstruction into Hounsfield
+	Units, using the material coefficients, photon energy photons and scale given."""
+	
+	# n_detectors is taken as an arguments to calculate the depth through which to calibrate the normalised water and air attenuation coefficients. For a ct_detect function that does not compensate for beam hardening, this value does not affect the result.
+	
+	# use water to calibrate, using the same calibration process as the normal CT data
+	size = 2.0*n_detectors*scale # size only matters if ct_detect attempts to compensate for beam hardening
+	water_residual = ct_detect(photons, material.coeff('Water'), size) # taking the residual intensity through water at a single depth of double the side length
+	air_residual = ct_detect(photons, material.coeff('Air'), size) # taking the residual intensity through air at a single depth of double the side length
+	mu_water = np.log(air_residual / water_residual) / size # performing calibration relative to residual energy through just air
+	# according to the above calibration, mu_air is 0
+	
+	# using result to convert to hounsfield units
+	reconstruction = 1000.0*(reconstruction - mu_water)/mu_water
+	
+	# clamping between -1024 and 3072
+	return np.clip(reconstruction, -1024.0, 3072.0)
