4cb2b7cf11f56d31e002173b83b1fd90d7741adf
Edmund Prager <pragere12@gmail.com>
2022-05-29 17:24:22 +0100

    Added completed DCM cropping function (in Visualisation folder) and added some visualisations of the scan as a whole and of the left forearm alone (using the low res data)

--- Visualisation/crop.py
+++ Visualisation/crop.py
@@ -1,7 +1,20 @@
 import numpy as np
 from create_dicom import *
+import pydicom
 
-def crop_dcms(directoryNprefix, n):
-	for i in range(n):
-		fileName = directoryNprefix + format(i + 1, '04d') + ".dcm"
-		print(fileName)
+def crop_dcms(inputDirectory, prefix, outputDirectory, crop_rad, crop_val, first, last):
+	for i in range(last - first + 1):
+		nString = format(i + first, '04d')
+		fileName = inputDirectory + prefix + nString + ".dcm"
+		dcm = pydicom.dcmread(fileName)
+		arr = dcm.pixel_array
+		(height, width) = arr.shape;
+		radSqd = crop_rad*crop_rad
+		for r in range(height):
+			dr = r - height*0.5
+			for c in range(width):
+				dc = c - width*0.5
+				if dr*dr + dc*dc > radSqd:
+					arr[r][c] = crop_val
+		dcm.PixelData = arr.tobytes()
+		dcm.save_as(outputDirectory + prefix + "cropped_" + nString + ".dcm")
