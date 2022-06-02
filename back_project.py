New dicoms without ring artifact and fixes Hounsfield unit conversion

back_project.py
@@ -3,7 +3,12 @@ import math
 import scipy
 from scipy import interpolate
 import sys
+import multiprocessing as mp
 
+def interpolate_and_process(x0, sino, ns, angles):
+    interpolated_func = scipy.interpolate.interp1d(np.arange(0, ns, 1), sino, kind='linear', copy=False,
+                                                   assume_sorted=True, bounds_error=False, fill_value=0, axis=0)
+    return interpolated_func(x0) * (np.pi / angles)
 
 def back_project(sinogram, skip=1):
 
@@ -78,22 +83,14 @@ def back_project_improved(sinogram, skip=1):
     # but the output coordinates need to be relative to the top left
     list_angles = np.array(range(angles))
     p = np.pi / 2 + list_angles * np.pi / angles
-    x0 = np.zeros((angles, n, n))
+    params = []
+
     for angle in range(angles):
-        x0[angle] = xi * np.cos(p[angle]) - yi * np.sin(p[angle]) + (ns / 2) - 0.5
+        params.append((xi * np.cos(p[angle]) - yi * np.sin(p[angle]) + (ns / 2) - 0.5,
+                      sinogram[angle], ns, angles))
 
-    sys.stdout.write("Reconstructing angles \n")
-    # interpolate and add this data to output
-    # remembering to multiply by dtheta as well as sum
-    interpolated_func = scipy.interpolate.interp1d(np.arange(0, ns, 1), sinogram, kind='linear', copy=False,
-                                                   assume_sorted=True, bounds_error=False, fill_value=0, axis=0)
-
-    sys.stdout.write("Done creating interpolation function \n")
-    interpolations = interpolated_func(x0)[:, :, list_angles]
-    sys.stdout.write("Done creating interpolations of size {} \n".format(interpolations.shape))
-    reconstruction = np.sum(interpolations, axis=2) * (np.pi / angles)
-    # for angle in range(angles):
-    #     reconstruction = reconstruction + interpolated_func(x0[angle])[:, angle] * (np.pi / angles)
+    with mp.Pool(processes=mp.cpu_count()) as pool:
+        reconstruction = np.sum(pool.starmap(interpolate_and_process, params), axis=0)
 
     # ensure any data outside the reconstructed circle is set to invalid
     reconstruction[np.where((xi**2 + yi**2) > (ns / 2) ** 2)] = -1

hu.py
@@ -1,12 +1,14 @@
-import numpy as np
-from attenuate import *
+
 from ct_calibrate import *
+from ct_our_functions import *
 
 # in order to turn to hounsfield units, we perform a linear regression by comparing with reconstructed data
-_points = ((0, -1000), (0.06585539958470409, 1450))
+# _points = ((0, -1000), (0.6144575495418269, 1450))
+#
+# _m = (_points[1][1] - _points[0][1]) / (_points[1][0] - _points[0][0])
+# _b = _points[0][1] - _m * _points[0][0]
 
-_m = (_points[1][1] - _points[0][1]) / (_points[1][0] - _points[0][0])
-_b = _points[0][1] - _m * _points[0][0]
+_mu_w = log_interp(np.array([0.04, 0.05]), np.array([0.2683, 0.2269]), 0.75 * 0.06)
 
 def hu(photons, material, reconstruction, scale, n_detectors, noise=True):
 	"""convert CT reconstruction output to Hounsfield Units
@@ -38,7 +40,13 @@ def hu_real(reconstruction, scale=None):
 	Units, using the material coefficients, photon energy photons and scale given."""
 
 	# using result to convert to hounsfield units
-	reconstruction = _m * reconstruction + _b
+	# reconstruction = _m * reconstruction + _b
+
+	# gotten from https://physics.nist.gov/PhysRefData/XrayMassCoef/ComTab/water.html and assuming a logarithmic linear
+	# relationship between energy and attenuation coefficient
+	# Average kev gotten from https://www.researchgate.net/figure/Average-kiloelectron-voltage-keV-given-the-kilovoltage-peak-kVp-7_tbl1_262228564
+
+	reconstruction = 1000.0 * (reconstruction - _mu_w) / _mu_w
 
 	# clamping between -1024 and 3072
 	return np.clip(reconstruction, -1024.0, 3072.0)

xtreme.py
@@ -8,7 +8,7 @@ from ramp_filter import *
 from ct_calibrate import *
 from back_project import *
 from create_dicom import *
-from hu import *
+import hu
 
 SAVE_DIR = os.path.join(os.getcwd(), 'dicoms')
 DATA_DIR = os.path.join(os.getcwd(), 'data')
@@ -264,7 +264,7 @@ class Xtreme(object):
 
         return Y
 
-    def reconstruct(self, slice, slice_min, slice_max, alpha):
+    def reconstruct_parallel(self, slice, slice_min, slice_max, alpha):
         '''
         Reconstruct a single slice of the fan-beam image.
 
@@ -275,9 +275,10 @@ class Xtreme(object):
         :return: Reconstructed CT image
         '''
         slice = np.clip(slice - slice_min, 1, None)
-        slice = ct_calibrate_real(slice, slice_max)
+        slice = ct_calibrate_real(slice, np.clip(slice_max - slice_min, 1, None))
         slice = self.fan_to_parallel(slice)
-        slice = ramp_filter(slice, self.scale, alpha)
+        # ramp filter expects a scale in cm but scale is in mm, so we multiply by 0.1
+        slice = ramp_filter(slice, self.scale * 0.1, alpha)
         image = back_project(slice)
         image = np.clip(image, a_min=0, a_max=np.max(image))
         return image
@@ -327,9 +328,9 @@ class Xtreme(object):
                         # reconstruct scan
                         print('Reconstructing scan angle {}'.format(scan))
                         slice, slice_min, slice_max = self.get_rsq_slice(scan)
-                        image = self.reconstruct(slice, slice_min, slice_max, alpha)
+                        image = self.reconstruct_parallel(slice, slice_min, slice_max, alpha)
 
-                        image = hu_real(image, self.scale)
+                        image = hu.hu_real(image, self.scale)
 
                         # save as dicom file
                         create_dicom(image,